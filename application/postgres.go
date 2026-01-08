45d6c43c8458be8dc2031772b341c93dccb8a253
Isaiah Cross <icross028@gmail.com>
2026-01-07 22:18:48 -0600

    format

--- application/postgres.go
+++ application/postgres.go
@@ -19,7 +19,6 @@ type PostgresConfig struct {
 func NewPostgresConnection(ctx context.Context, cfg PostgresConfig) (*pgxpool.Pool, error) {
 	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
 
-	// db, err := pgx.Connect(ctx, connectionString)
 	pool, err := pgxpool.New(ctx, connectionString)
 	if err != nil {
 		log.Fatalf("failed to connect to postgres: %v", err)
