9eb2b827f2424ee3270e29de021b7c1ef05d5c16
Isaiah Cross <icross028@gmail.com>
2026-01-11 01:50:50 -0600

    Wrote an auth middleware handler\?

--- middleware/jwt_auth.go
+++ middleware/jwt_auth.go
@@ -1,11 +1,31 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	"crosssystems.co/uptime-go-be/auth"
+)
 
 func JwtAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("access_token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
 
+		token := cookie.Value
+
+		if auth.VerifyJwtToken(token) != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
 		
+		// access granted
 		next.ServeHTTP(w, r)
 	})
 }
